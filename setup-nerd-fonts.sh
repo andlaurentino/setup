f31de43239c3ccaa236b798d6e3b4259379571d9
Anderson Laurentino <anderson@laurentino.me>
2024-03-17 15:20:36 -0300

    fix: change the nerd fonts intallation path

--- setup-nerd-fonts.sh
+++ setup-nerd-fonts.sh
@@ -4,5 +4,4 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.zip
 unzip JetBrainsMono.zip
 unzip Meslo.zip
 
-cp ./JetBrainsMono/*.ttf ~/Library/Fonts/
-cp ./Meslo/*.ttf ~/Library/Fonts/
+mv *.ttf ~/Library/Fonts
