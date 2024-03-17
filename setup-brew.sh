1f176517fa30d86d651b1ac5e6d76a1cfa28bcef
Anderson Laurentino <anderson@laurentino.me>
2024-03-16 23:50:56 -0300

    formated

--- setup-brew.sh
+++ setup-brew.sh
@@ -1,6 +1,6 @@
 if ! command -v brew &> /dev/null
 then
-    echo "brew could not be found"
+	echo "brew could not be found"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
